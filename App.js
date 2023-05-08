1df2b7cf64f946524eb3a9b3273d84c7d1a23789
Oleg Tkach <KratosHome101@gmail.com>
2023-05-08 09:39:01 +0300

    Ing buttom TouchableHighlight

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Alert, Button, Image, SafeAreaView, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Доровенькі були</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const openModal = () => {
+        Alert.alert("first modal", "main masseg", [
+            {
+                text: "yesy",
+                onPress: () => console.log("yes")
+            },
+            {
+                text: "yesy"
+            }
+        ])
+    }
+    const openModalNext = () => {
+        Alert.prompt("first modal", "main masseg", text => console.log(text))
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Доровенькі були</Text>
+            <Button title={"кнопdка"} onPress={openModal}/>
+            <Button title={"кнопка два "} onPress={openModalNext}/>
+            <TouchableHighlight onPress={openModalNext}>
+                <Image source={require("./assets/adaptive-icon.png")}/>
+            </TouchableHighlight>
+            <StatusBar style="auto"/>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: "red",
+    },
 });
